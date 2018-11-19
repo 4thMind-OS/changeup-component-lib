6c86b9db175296660991cd57fe1401bc1997358a
WheatleyTheCore <alex.colwell@icloud.com>
2018-11-19 18:32:05 -0500

    added coin

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { render } from "react-dom";
-import { Input } from "./lib";
+import { Nickel, Quarter, Dime } from "./lib";
 
 const App = () => (
   <div style={{ width: 640, margin: "15px auto" }}>
     <h1>Hello React</h1>
-    <Input />
+    <Nickel />
+    <Quarter />
+    <Dime />
   </div>
 );
 
---
+++ src/lib/Nickel/Nickel.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Nickel.css"
+
+const Nickel = () => {
+    return (
+        <div className="Nickel">
+            <div className="Nickel-text">
+                5¢
+            </div>
+        </div>
+    )
+}
+
+export default Nickel
