201933edd70113143e9e547e9d2cc1b6ee941792
WheatleyTheCore <alex.colwell@icloud.com>
2018-11-21 19:26:37 -0500

    added description component

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,6 +1,7 @@
 import Input from "./TextInput";
-import Nickel from "./Nickel/Nickel"
-import Quarter from "./Quarter/Quarter"
-import Dime from "./Dime/Dime"
+import Nickel from "./Coins/Nickel/Nickel"
+import Quarter from "./Coins/Quarter/Quarter"
+import Dime from "./Coins/Dime/Dime"
 import Bounty from "./Bounty/Bounty"
-export { Input, Nickel, Quarter, Dime, Bounty };
+import Description from "./Description/Description"
+export { Input, Nickel, Quarter, Dime, Bounty, Description };
